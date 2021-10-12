Fixed FA by enabling batch norm and fixing averaging procedure

core/datasets.py
@@ -4,7 +4,8 @@ from os.path import join
 import numpy as np
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, Subset
-
+from typing import List
+from PIL import Image
 
 
 def split_dirichlet(labels, n_clients, alpha, double_stochstic=True, seed=0, **kwargs):
@@ -123,6 +124,16 @@ class LocalDataset(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self._test_data, batch_size=self.batch_size, num_workers=10, pin_memory=True)
+
+class SyntheticAnomalyDataset(LocalDataset):
+    def __init__(self, anomaly_class_labels: List[int], data_dir: str = "datasets", batch_size: int = 32, split: str ='dirichlet', n_clients: int = 10, transforms=None, **split_kwargs):
+        super().__init__(data_dir=data_dir, batch_size=batch_size, split=split, n_clients=n_clients, transforms=transforms, **split_kwargs)
+        self.anomaly_class_labels = anomaly_class_labels
+
+    def setup(self, stage=None):
+        anomaly_data = Subset(self.train_data, self.train_data.targets)
+
+
 
 
 class CIFAR10DataModule(LocalDataset):

core/models.py
@@ -5,6 +5,7 @@ import pytorch_lightning as pl
 import numpy as np
 from collections import OrderedDict
 from pl_bolts.models.self_supervised import SimCLR
+from torchmetrics.classification.accuracy import Accuracy
 
 class Model(pl.LightningModule):
     def __init__(self, role , id=0,  **kwargs):
@@ -14,7 +15,7 @@ class Model(pl.LightningModule):
         self.datasets = {}
         self._mode = "local"
         self.id = id
-        self.accuracy = pl.metrics.Accuracy()
+        self.accuracy = Accuracy()
 
     def train_dataloader(self):
         if self._mode == "local":
@@ -44,6 +45,7 @@ class Model(pl.LightningModule):
             return self.classification_layer(features)
 
     def local_training(self, batch, batch_idx):
+        self.train()
         x, y = batch
         loss = F.cross_entropy(self(x), y)
         self.log(f'{self.role}_{self.id}/train_loss/local', loss, on_step=False, on_epoch=True)
@@ -63,14 +65,11 @@ class Model(pl.LightningModule):
         self.train()
         x, y = batch
         out = self.forward(x)
-        print(out)
         y_hat = torch.argmax(out, dim=1)
-        print(y_hat)
         self.accuracy(y_hat, y)
 
     def test_epoch_end(self, outputs):
         self.log('accuracy', self.accuracy.compute())
-
 
     def configure_optimizers(self):
         optimizer = self.kwargs[f"optimizer_{self.role}"](self.parameters(), lr=self.kwargs[f"lr_{self.role}"])
@@ -107,7 +106,7 @@ class VGG(Model):
             apply_gn(self)
 
 
-    def make_layers(self, cfg, batch_norm=False):
+    def make_layers(self, cfg, batch_norm=True):
         layers = []
         in_channels = 3
         for v in cfg:

core/steps.py
@@ -4,6 +4,7 @@ import pytorch_lightning as pl
 from pl_bolts.datamodules import STL10DataModule
 from pl_bolts.models.self_supervised.simclr.transforms import SimCLRTrainDataTransform, SimCLREvalDataTransform
 from .models import ExtendedSimCLR
+from torch.optim.swa_utils import update_bn
 
 from os.path import join
 
@@ -22,15 +23,14 @@ def Distribute_Dataset(to, dataset, name, train=True, test=True):
 def Distribute_Model(source, target):
     def Distribute_Model2(message=None):
         for model in target:
-            for t_param, s_param in zip(model.parameters(), source[0].parameters()):
-                t_param.detach().copy_(s_param.detach())
+            model.load_state_dict(source[0].state_dict())
         return message
     return Distribute_Model2
 
 def Train(on, epochs, mode, logger=None):
     def Train2(message=None):
         for model in on:
-            pl.Trainer(max_epochs=epochs, logger=logger, weights_summary=None, gpus=1).fit(model.mode(mode))
+            pl.Trainer(max_epochs=epochs, logger=logger, weights_summary=None).fit(model.mode(mode))
         return message
     return Train2
 
@@ -50,17 +50,15 @@ def Pretrain(on, epochs, dataset, logger=None):
 def Test(on, logger=None):
     def Test2(message=None):
         for model in on:
-            pl.Trainer(logger=logger, gpus=1).test(model.mode("local"))
+            pl.Trainer(logger=logger).test(model.mode("local"))
         return message
     return Test2
 
 def Aggregate(source, target, type='model', method='Average'):
     def Aggregate2(message=None):
-        if type == 'model':
-            for target_model in target:
-                for t_param, s_params in zip(target_model.parameters(), zip(*[s.parameters() for s in source])):
-                    print(len(s_params))
-                    t_param.detach().copy_(torch.mean(torch.stack([s.detach() for s in s_params]),0))
+        for target_model in target:
+            for p_target, *p_source in zip(target_model.parameters(), *[model.parameters() for model in source]):
+                p_target.detach().copy_(torch.mean(torch.stack([p_model.detach() for p_model in p_source]), dim=0))
         return message
     return Aggregate2
 