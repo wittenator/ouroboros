Reverted changes and fixed averaging

core/steps.py
@@ -48,7 +48,8 @@ def Train(on: List[Participant], epochs: int, device: torch.device, batch_size: 
             model = participant.model,
             device = device,
             dataset = participant.datasets["train"],
-            batch_size = batch_size
+            batch_size = batch_size,
+            number_of_classes = participant.number_of_classes,
             )  # type: ignore
         for epoch in trange(epochs):
             epoch_acc = trainer_instance.train_for_one_epoch()
@@ -65,14 +66,15 @@ def Validate(on, device, batch_size, communication_round: int, logger: SummaryWr
             model = participant.model,
             device = device,
             dataset = participant.datasets["validate"],
-            batch_size = batch_size
+            batch_size = batch_size,
+            number_of_classes = participant.number_of_classes,
         ).validate()
 
         logger.add_scalar(f'val/{participant.group}/{participant.group}-{participant.id}/accuracy', acc, communication_round)
 
 
 def Aggregate(sources, target, aggregator: ModelConsolidationStrategy):
-    aggregator.consolidate_models(target_model=target.model, sources=[source.model for source in sources])
+    aggregator.consolidate_models(target_model=target.model, source_models=[source.model for source in sources])
 
 def Train_and_Aggregate(
     on: List[Participant],

core/utils.py
@@ -10,18 +10,6 @@ from dataclasses import dataclass, field
 
 from .lifecycle import ModelTrainer, ModelValidator
 
-class ModelPersistanceQueue:
-    self.queue = []
-    self.maxlen = 2
-
-    def put(self, model):
-        if len(self.queue) > self.maxlen:
-            torch.save(self.queue[-1].state_dict(), f"./tmp/test-0.pt")
-            
-
-class ModelPersistanceDescriptor:
-
-
 @dataclass
 class Participant:
     """Class for keeping track of an item in inventory."""
@@ -31,6 +19,7 @@ class Participant:
     model: nn.Module = None
     id: Any = None
     datasets: Dict = field(default_factory=dict)
+    number_of_classes: int = None
 
 def split_dirichlet(labels, n_clients, alpha, double_stochstic=True, **kwargs):
     '''Splits data among the clients according to a dirichlet distribution with parameter alpha'''