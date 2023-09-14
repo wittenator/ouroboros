e10e9827a1726685624e943d1cb3854dde182f81
wittenator <>
2023-09-14 08:08:21 +0200

    Added fixes

--- core/lifecycle.py
+++ core/lifecycle.py
@@ -155,6 +155,103 @@ class ClassificationModelTrainer(ModelTrainer):
         metric.reset()
         return acc.item()
 
+class DetectionModelTrainer(ModelTrainer):
+    """Represents a model trainer, which trains a object detection model."""
+
+    def __init__(
+            self,
+            model: torch.nn.Module,
+            optimizer: Any,
+            device: Union[str, torch.device],
+            dataset: torch.utils.data.Dataset,  # type: ignore
+            batch_size: int,
+            learning_rate: float = 0.0005,
+            number_of_classes: int = 10,
+            ) -> None:
+        """Initializes a new ReconstructionBasedAnomalyDetectionModelTrainer instance.
+
+        Args:
+            model (torch.nn.Module): The reconstruction-based anomaly detection model that is to be trained.
+            device (Union[str, torch.device]): The device on which the training is performed.
+            training_subset (torch.utils.data.Dataset): The training split of the dataset on which the model is to be trained.
+            batch_size (int): The size of the mini-batch used during training.
+            learning_rate (float): The initial learning rate of the optimizer.
+            weight_decay (float): The factor for the L2 regularization that is added by the optimizer to promote smaller weights.
+            model_name (str): The name of the model that is to be trained.
+        """
+
+        # Stores the arguments for later use
+        self.model = model
+        self.device = device
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.number_of_classes = number_of_classes
+
+
+        # Creates the data loaders
+        self.training_data_loader = torch.utils.data.DataLoader(  # type: ignore
+            self.dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            pin_memory=True,
+            num_workers=4
+        )
+
+        # Moves the model to the specified device
+        self.model = self.model.to(self.device)
+
+        # Creates the optimizer for the training
+        self.optimizer = optimizer(
+            self.model.parameters(),
+            lr=self.learning_rate,
+            weight_decay=0.1
+        )
+
+    def train_for_one_epoch(self) -> float:
+        """Trains the reconstruction-based anomaly detection model for a single epoch.
+
+        Returns:
+            float: Returns the average training loss over the epoch.
+        """
+
+        # Puts the model into training mode, because some parts of the model may behave differently in training as compared to
+        # validation/testing/inference
+        self.model.train()
+
+        # Cycles through the entire training subset of the dataset and trains the model on the samples (this is for one epoch of training)
+        metric = torchmetrics.detection.mean_ap.MeanAveragePrecision().to(self.device)
+        for inputs, bboxes in self.training_data_loader:
+            self.model.train()
+
+            # Resets the gradients of the optimizer (otherwise the gradients would accumulate)
+            self.optimizer.zero_grad(set_to_none=True)
+
+            # Moves the inputs to the selected device
+            inputs = inputs.to(self.device, non_blocking=True)
+            bboxes = bboxes.to(self.device, non_blocking=True)
+
+            # Performs a forward pass through the neural network
+            with torch.autocast(device_type=self.device.type):
+                loss = self.model(images=inputs, targets=bboxes)
+
+            # Performs the backward pass and the optimization step
+            loss.backward()
+            self.optimizer.step()
+
+            # Updates the sum of losses from which the mean loss will be computed
+            self.model.eval()
+            with torch.no_grad():
+                detections = self.model(images=inputs, targets=bboxes)
+                acc = metric(detections, bboxes)
+
+        # metric on all batches using custom accumulation
+        acc = metric.compute()
+
+        # Reseting internal state such that metric ready for new data
+        metric.reset()
+        return acc.item()
+
 
 class ClassificationModelValidator(ModelValidator):
     """Represents a model validator, which validates a reconstruction-based anomaly detection model on the validation subset of the dataset."""
@@ -230,6 +327,82 @@ class ClassificationModelValidator(ModelValidator):
 
                 # Updates the validation metrics
                 acc = metric(class_probs.argmax(axis=1), classes)
+
+            # metric on all batches using custom accumulation
+            acc = metric.compute()
+
+            # Reseting internal state such that metric ready for new data
+            metric.reset()
+            return acc.item()
+
+class DetectionModelValidator(ModelValidator):
+    """Represents a model validator, which validates a reconstruction-based anomaly detection model on the validation subset of the dataset."""
+
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        device: Union[str, torch.device],
+        dataset: torch.utils.data.Dataset,  # type: ignore
+        batch_size: int
+        ) -> None:
+        """Initializes a new ReconstructionBasedAnomalyDetectionModelValidator instance.
+
+        Args:
+            model (torch.nn.Module): The reconstruction-based anomaly detection model that is to be validated.
+            device (Union[str, torch.device]): The device on which the validation is performed.
+            validation_subset (torch.utils.data.Dataset): The validation split of the dataset on which the model is to be validated.
+            batch_size (int): The size of the mini-batch used during validation.
+            model_name (str): The name of the model that is to be validated.
+        """
+
+        # Stores the arguments for later use
+        self.model = model
+        self.device = device
+        self.validation_subset = dataset
+        self.batch_size = batch_size
+
+        # Creates the data loaders
+        self.validation_data_loader = torch.utils.data.DataLoader(
+            self.validation_subset,
+            batch_size=self.batch_size,
+            pin_memory=True,
+            shuffle=True,
+            num_workers=4
+        )
+
+        # Moves the model to the specified device
+        self.model = self.model.to(self.device)
+
+    def validate(self) -> float:
+        """Validates the model on the validation subset of the dataset.
+
+        Returns:
+            tuple[float, float, float, float, float]: Returns a tuple containing the validation loss, the AUC, the mean anomaly score of the inliers,
+                the mean anomaly score of the outliers, and the p-value of the Mann-Whitney U test, which determines how significant the difference
+                between the anomaly scores of the inliers and outliers is, i.e., the lower the p-value, the more significant is the difference between
+                the anomaly scores of the inliers and outliers.
+        """
+
+        # Puts the model into evaluation mode, because some parts of the model may behave differently in training as compared to
+        # validation/testing/inference
+        self.model.eval()
+
+        # Since we are only evaluating the model, the gradient does not have to be computed
+        with torch.no_grad():
+
+            # Cycles through the whole validation subset of the dataset and performs the validation
+            metric = torchmetrics.detection.mean_ap.MeanAveragePrecision().to(self.device)
+            for inputs, bboxes in self.validation_data_loader:
+
+                # Transfers the batch to the selected device
+                inputs = inputs.to(self.device, non_blocking=True)
+                bboxes = bboxes.to(self.device, non_blocking=True)
+
+                # Performs the forward pass through the neural network
+                detections = self.model(inputs)
+
+                # Updates the validation metrics
+                acc = metric(detections, bboxes)
 
             # metric on all batches using custom accumulation
             acc = metric.compute()
